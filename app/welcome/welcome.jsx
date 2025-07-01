d8c3f2fb17b5a2c6a9bb152c8007355abe27c6d1
Brayden Emerson <braydenjames2004@gmail.com>
2025-07-01 13:15:28 -0500

    Added Navigation Menu to Home page

--- app/welcome/welcome.jsx
+++ app/welcome/welcome.jsx
@@ -1,10 +1,13 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 import SplitText from "../../src/TextAnimations/SplitText/SplitText"
+import React from 'react';
+import Navigation from "./Navigation";
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
+      <Navigation />
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
 
         <header className="flex flex-col items-center gap-9">
